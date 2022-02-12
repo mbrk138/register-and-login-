52a3bc46903454310dd8df59e19349d6b4d3e996
baghermohammad@hotmail.com <baghermohammad@hotmail.com>
2022-02-12 22:13:48 +0330

    first commit

---
+++ Infrastructure/Persis/DateBassDbContext.cs
@@ -0,0 +1,20 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Domain.Models;
+using Microsoft.AspNet.Identity.EntityFramework;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace Infrastructure.Persis
+{
+    public class DateBassDbContext : IdentityDbContext<User>
+    {
+        public DateBassDbContext(DbContextOptions<DateBassDbContext> options) : base(options)
+        {
+            
+        }
+
+    }
+}
